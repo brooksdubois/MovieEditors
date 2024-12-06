85437734a31e7a639a9cb92282a7465ae0621e99
Brooks DuBois <brooksdubois@mac.lan>
2024-12-05 20:25:30 -0500

    Refactoring for testablility

---
+++ src/MovieDBApiClient.ts
@@ -0,0 +1,73 @@
+import {pluck} from "ramda";
+
+interface DiscoverMovie {
+    id: number,
+    title: string,
+    release_date: string,
+    vote_average: string,
+}
+
+interface DiscoverResponse {
+    results: [DiscoverMovie]
+}
+
+interface EditorsById {
+    id: number
+    editors: [string]
+}
+
+interface CastMember {
+    name: string
+    known_for_department: string
+}
+interface CreditsResponse {
+    id: number
+    cast:[CastMember]
+}
+
+export default class MovieDBApiClient {
+    fetchOptions: object
+    baseURL = "https://api.themoviedb.org/3"
+
+    constructor(apiKey: string) {
+        this.fetchOptions = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer ' + apiKey
+            }
+        };
+    }
+
+    fetchMovies = async (year: number) => {
+        const discoverUrl = `${this.baseURL}/discover/movie?include_adult=false&include_video=false&primary_release_year=${year}&language=en-US&page=1&sort_by=popularity.desc`;
+        const discoverResponse = await fetch(discoverUrl, this.fetchOptions)
+        const discoverJson = await discoverResponse.json() as DiscoverResponse
+        return discoverJson.results
+    }
+
+    fetchCrewForAllMovies = async(movieResults: DiscoverMovie[]) => {
+        const movieIdUrls = movieResults.map((it: DiscoverMovie) => `${this.baseURL}/movie/${it.id}/credits`)
+        const movieFetches = movieIdUrls.map(async url => {
+            const response = await fetch(url, this.fetchOptions)
+            return await response.json() as CreditsResponse
+        })
+        return await Promise.all(movieFetches)
+    }
+
+    filterByEditors = (movieCredits: CreditsResponse[]) =>
+        movieCredits.map((it: CreditsResponse)=> {
+            const editorsObjects = it.cast.filter(castMember =>
+                castMember.known_for_department === "Editing"
+            )
+            const editors = pluck("name")(editorsObjects)
+            return { id: it.id, editors } as EditorsById
+        })
+
+   mapMovieResponsesToEditors = (movieResults: DiscoverMovie[], editorsById: EditorsById[]) =>
+        movieResults.map((movie: DiscoverMovie) => {
+            const { id, title, release_date, vote_average } = movie
+            const { editors } = editorsById.find(it => it.id == id) ?? { editors: [] }
+            return { title, release_date, vote_average, editors }
+        })
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,83 +1,20 @@
 import fastifyPlugin from "fastify-plugin";
 import {FastifyInstance, FastifyReply, FastifyRequest, FastifyServerOptions} from "fastify";
 import { yearSchema } from "../schemas/year.js"
-import { pluck } from "ramda";
+import MovieDBApiClient from "../MovieDBApiClient";
 
 interface YearBody {
     year: number
 }
-
-interface DiscoverMovie {
-    id: number,
-    title: string,
-    release_date: string,
-    vote_average: string,
-}
-
-interface EditorsById {
-    id: number
-    editors: [string]
-}
-
-interface DiscoverResponse {
-    results: [DiscoverMovie]
-}
-
-interface CastMember {
-    name: string
-    known_for_department: string
-}
-interface CreditsResponse {
-    id: number
-    cast:[CastMember]
-}
-
-interface ApiResponse {
-    title: string,
-    release_date: string,
-    vote_average: string,
-    editors: [string]
-}
-
 async function indexRoutes(server: FastifyInstance, options: FastifyServerOptions) {
-    server.post("/",
-        { schema: yearSchema},
-        async (request: FastifyRequest, reply:FastifyReply) => {
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer ' + server.config.API_TOKEN
-            }
-        };
-        const body = request.body as YearBody
-        const discoverUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&primary_release_year=${body.year}&language=en-US&page=1&sort_by=popularity.desc`;
 
-
-        const discoverResponse = await fetch(discoverUrl, options)
-        const { results: discoverJson } = await discoverResponse.json() as DiscoverResponse
-
-        const movieIdUrls = discoverJson.map((it: DiscoverMovie) => `https://api.themoviedb.org/3/movie/${it.id}/credits`)
-
-        const movieCredits = await Promise.all(
-            movieIdUrls.map(async url => {
-                const response = await fetch(url, options)
-                return await response.json()
-            })
-        )
-        const editorsById = movieCredits.map((it: CreditsResponse)=> {
-            const editorsObjects = it.cast.filter(castMember =>
-                castMember.known_for_department === "Editing"
-            )
-            const editors = pluck("name")(editorsObjects)
-            return { id: it.id, editors }
-        })
-
-        return discoverJson.map(movie => {
-            const { id, title, release_date, vote_average } = movie
-            const { editors } = editorsById.find(it => it.id == id) ?? { editors: [] }
-            return { title, release_date, vote_average, editors }
-        })
+    server.post("/", { schema: yearSchema}, async (request: FastifyRequest, reply:FastifyReply) => {
+        const { year } = request.body as YearBody
+        const movieDBClient = new MovieDBApiClient(server.config.API_TOKEN)
+        const movieResults= await movieDBClient.fetchMovies(year)
+        const movieCredits = await movieDBClient.fetchCrewForAllMovies(movieResults)
+        const editorsById = movieDBClient.filterByEditors(movieCredits)
+        return movieDBClient.mapMovieResponsesToEditors(movieResults, editorsById)
     });
 }
 
